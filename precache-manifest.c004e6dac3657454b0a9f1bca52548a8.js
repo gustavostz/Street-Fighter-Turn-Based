63f37eb2fb3f1bf8133f46cdfce3769967b1e9cb
Gustavo Steinmetz <stzgustavo@gmail.com>
2020-11-27 13:48:04 -0300

    Updates

---
+++ precache-manifest.c004e6dac3657454b0a9f1bca52548a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdee9aa7bdceac51604a00ffbdc264e3",
+    "url": "/projects/street-fighter/index.html"
+  },
+  {
+    "revision": "9c6590b9419c5a240392",
+    "url": "/projects/street-fighter/static/css/main.fa2809ac.chunk.css"
+  },
+  {
+    "revision": "302ad57e789251c5e37d",
+    "url": "/projects/street-fighter/static/js/2.67bfa89c.chunk.js"
+  },
+  {
+    "revision": "9c6590b9419c5a240392",
+    "url": "/projects/street-fighter/static/js/main.e61264e2.chunk.js"
+  },
+  {
+    "revision": "b60f4e6baec5d243e2d6",
+    "url": "/projects/street-fighter/static/js/runtime-main.2e0a6d49.js"
+  },
+  {
+    "revision": "5b4df42120e45f5145bf142d59bef0ad",
+    "url": "/projects/street-fighter/static/media/BG4.5b4df421.png"
+  },
+  {
+    "revision": "86b41660eaca0da44f586e4675b178e6",
+    "url": "/projects/street-fighter/static/media/VS.86b41660.png"
+  }
+]);
