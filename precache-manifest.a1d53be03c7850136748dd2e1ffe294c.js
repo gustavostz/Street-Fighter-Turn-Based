eb7c3c05cd9fdbaf8647d398146cfd67e15b9357
Gustavo Steinmetz <stzgustavo@gmail.com>
2020-11-29 20:16:40 -0300

    Updates

---
+++ precache-manifest.a1d53be03c7850136748dd2e1ffe294c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e3c19ebe577281a0296d444f51a1c64",
+    "url": "/Street-Fighter-Turn-Based/index.html"
+  },
+  {
+    "revision": "7e15eaeb3afb54b9c26c",
+    "url": "/Street-Fighter-Turn-Based/static/css/main.5eef6fb1.chunk.css"
+  },
+  {
+    "revision": "302ad57e789251c5e37d",
+    "url": "/Street-Fighter-Turn-Based/static/js/2.67bfa89c.chunk.js"
+  },
+  {
+    "revision": "7e15eaeb3afb54b9c26c",
+    "url": "/Street-Fighter-Turn-Based/static/js/main.2f7da860.chunk.js"
+  },
+  {
+    "revision": "7f29d5d4e4fab030c6e0",
+    "url": "/Street-Fighter-Turn-Based/static/js/runtime-main.db04ec26.js"
+  },
+  {
+    "revision": "5b4df42120e45f5145bf142d59bef0ad",
+    "url": "/Street-Fighter-Turn-Based/static/media/BG4.5b4df421.png"
+  },
+  {
+    "revision": "86b41660eaca0da44f586e4675b178e6",
+    "url": "/Street-Fighter-Turn-Based/static/media/VS.86b41660.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.441af3c451c211928b2cced9218afeb6.js"
+  "/Street-Fighter-Turn-Based/precache-manifest.a1d53be03c7850136748dd2e1ffe294c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Street-Fighter-Turn-Based/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
